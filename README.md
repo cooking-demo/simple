26553961f8ba44453d6a4f2f6d7b75e2ba0452f9
qingwei.li <cinwell.li@gmail.com>
2016-09-10 01:51:21 +0800

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+# cooking simple demo
+
+## Demo
+https://cooking-demo.github.io/simple
+
+## Development
+```shell
+npm i
+npm run dev
+```
+
+## Production
+```shell
+npm run dist
+```
+
+## LICENSE
+WTFPL
---
+++ cooking.conf.js
@@ -0,0 +1,25 @@
+var cooking = require('cooking');
+
+cooking.set({
+  entry: {
+    app: './src/index.js',
+    vendor: ['vue']
+  },
+  dist: './dist',
+  clear: true, // 每次打包都清理掉 dist 目录
+  hash: true,
+  sourceMap: true,
+  template: './index.html',
+  devServer: { port: 8888, publicPath: '/' },
+  postcss: [
+    require('postcss-salad')
+  ],
+  extractCSS: true, // 提取 CSS 文件
+  chunk: [
+    'vendor', // entry 里定义的入口文件，也就是会将 vue 单独打包
+    'manifest' // 这个并没有在 entry 里声明的会将所有公用部分打包，也就是 webpack runtime
+  ],
+  extends: ['vue2', 'lint']
+});
+
+module.exports = cooking.resolve();
